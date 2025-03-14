e1d3d1e4fe844b9cb8016d8eec4a02e72fde8b96
Sem8 <mssemmi8@gmail.com>
2025-03-14 00:16:22 -0400

    - Adds PUT and DELETE functionality in FE

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState(0);
+  const [newTitle, setNewTitle] = useState("");
 
   useEffect(() => {
     fetchBooks();
@@ -24,41 +25,92 @@ function App() {
   const addBook = async () => {
     const bookData = {
       title,
-      release_year: releaseYear
-    }
+      release_year: releaseYear,
+    };
 
     try {
-      const respponse = await fetch('http://127.0.0.1:8000/api/books/create/', {
-        method: 'POST',
+      const respponse = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(bookData)
+        body: JSON.stringify(bookData),
       });
-      const data = await respponse.json();     
-      // console.log(data); 
-      setBooks(prev => [...prev, data])
+      const data = await respponse.json();
+      // console.log(data);
+      setBooks((prev) => [...prev, data]);
     } catch (err) {
-      console.error(err)
+      console.error(err);
+    }
+  };
+
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year
+    };
+
+    try {
+      const respponse = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+      const data = await respponse.json();
+      // console.log(data);
+      setBooks((prev) => prev.map((book) => {
+        if (book.id == pk) {
+          return data;
+        } else {
+          return book
+        }
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const deleteBook = async (pk) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "DELETE",
+    });
+    setBooks(prev => prev.filter(book => book.id !== pk))
+      
+    } catch (error) {
       
     }
-
   }
 
   return (
     <>
       <h1>Book Website</h1>
       <div>
-        <input type="text" placeholder="Book Title...." onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Release date...." onChange={(e) => setReleaseYear(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Book Title...."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Release date...."
+          onChange={(e) => setReleaseYear(e.target.value)}
+        />
         <button onClick={addBook}>Add Book</button>
       </div>
       {books.map((book) => (
         <div key={book.id}>
           <p>Title: {book.title}</p>
           <p>Release Year: {book.release_year} </p>
-          <input type="text" placeholder="New Title..." />
-          <button> Change Title</button>
+          <input
+            type="text"
+            placeholder="New Title..."
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateTitle(book.id, book.release_year)}> Change Title</button>
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
         </div>
       ))}
     </>
